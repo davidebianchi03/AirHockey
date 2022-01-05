65c78752d8a51b425da0045611ee01bd0cb2a638
Davide D'Assaro <xdado355@gmail.com>
2022-01-05 13:25:49 +0100

    Creazione classe per aggiornamento manopola avversario
    Bozza incompleta della classe di aggiornamento manopola

---
+++ AirHockey/AirHockey/HandleUpdate.cs
@@ -0,0 +1,32 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using window_utilities;
+
+namespace AirHockey
+{
+    class HandleUpdate
+    {
+        private int x, y;
+
+        private void MessageReceived(object sender, MessageReceivedArgs e)
+        {
+            SharedSettings settings = SharedSettings.GetInstance();
+            SendAndReceive sendAndReceive = settings.sendAndReceive;
+            if (e.message.Command != null)
+            {
+                //controllo che il comando sia quello della connessione
+                if (e.message.Command == "m")
+                {
+                    //Se il comando è quello di richiesta della connessione
+                    //salvo le coordinate che mi sono state mandate
+                    string[] coordinate = e.message.Body.Split(';');
+                    x = Int32.Parse(coordinate[0]);
+                    y = Int32.Parse(coordinate[1]);
+                }
+                
+            }
+        }
+    }
+}
